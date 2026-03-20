8e529adfcc0fca3b70bbd9315ae104343f7e300c
Sanjay Sivapragasam <sanjaysivapragasam12@gmail.com>
2026-03-20 16:50:25 -0400

    update comments and formatting

--- services/notification/main.py
+++ services/notification/main.py
@@ -20,19 +20,27 @@ def on_message(channel, method, properties, body):
     channel.basic_ack(delivery_tag=method.delivery_tag)
 
 def start_consumer():
-    while True:
+    while True: # run forever, since any consumer service always listens
         try:
             print("[Notification] Connecting to RabbitMQ...")
+            # set up physical connection
             connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
+            # create channel (virtual connection) and declare the queue
             channel = connection.channel()
+            # queue will survive restart with durable = true so messages aren't lost
             channel.queue_declare(queue="reservation_events", durable=True)
+            # request 1 message at a time
             channel.basic_qos(prefetch_count=1)
+            
+            # register the callback
             channel.basic_consume(queue="reservation_events", on_message_callback=on_message)
             print("[Notification] Waiting for messages...")
+            # start the consuming loop
             channel.start_consuming()
         except pika.exceptions.AMQPConnectionError:
+            # throw error is RabbitMQ isn't ready yet
             print("[Notification] RabbitMQ not ready, retrying in 5s...")
-            time.sleep(5)
+            time.sleep(5) # wait for 5 seconds then try again, to avoid crashing
 
 # a Python convention, that means only run file is file is run directly and not imported by another file
 # standard way to start python script, since there is n FastAPI connection here
