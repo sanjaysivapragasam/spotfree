f45f27dec99ee8d338dcfdc41814b69d157633a1
Sanjay Sivapragasam <sanjaysivapragasam12@gmail.com>
2026-03-20 16:38:25 -0400

    configure notification service

--- services/notification/main.py
+++ services/notification/main.py
@@ -3,11 +3,20 @@ import json
 import time
 import os
 
-RABBITMQ_URL = os.environ.get("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
+# configure RabbitMQ
+RABBITMQ_URL = os.environ.get(
+    "RABBITMQ_URL", "amqp://guest:guest@localhost:5672/"
+    #guest:guest is RabbitMQ's default username and password
+    # amqp = advanced message queing Protocol
+    # 5672 is default port
+)
 
+# automatically called by pika every time a message arrives
+# aka a callback function, and RabbitMQ calls it
 def on_message(channel, method, properties, body):
-    data = json.loads(body)
+    data = json.loads(body) # body is raw message in bytes, json.loads converts to a python dictionary
     print(f"[Notification] Event received: {data}")
+    # delivery tag is an ID for each message so RabbitMQ knows which message is being ACK
     channel.basic_ack(delivery_tag=method.delivery_tag)
 
 def start_consumer():
@@ -25,5 +34,7 @@ def start_consumer():
             print("[Notification] RabbitMQ not ready, retrying in 5s...")
             time.sleep(5)
 
+# a Python convention, that means only run file is file is run directly and not imported by another file
+# standard way to start python script, since there is n FastAPI connection here
 if __name__ == "__main__":
     start_consumer()
